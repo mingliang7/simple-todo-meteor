c598fb1826b4c8cafd3a74a1992355a327bfb37a
kevin <faithful.7up@gmail.com>
2016-05-01 14:06:49 +0700

    add data

--- server/main.js
+++ server/main.js
@@ -3,4 +3,9 @@ import {Tasks} from '../imports/api/task.js'
 
 Meteor.startup(() => {
   // code to run on server at startup
+  if(Tasks.find().count() <=0 ){
+    for(let i = 0 ; i < 10; i++){
+      Tasks.insert({_id: `${i}`, text: `text ${i}`})
+    }
+  }
 });
